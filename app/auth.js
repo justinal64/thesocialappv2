1e08089cd13de9098fde1492bab3026c785e2227
justin leggett <justin@codingrookie.com>
2017-06-12 20:57:19 -0500

    naviagtion and I am passing the values to check auth

--- app/auth.js
+++ app/auth.js
@@ -2,6 +2,9 @@ import { AsyncStorage } from "react-native";
 
 export const USER_KEY = "auth-demo-key";
 
+export const auth = (email, password) =>
+  console.log("email = ", email, " password = ", password);
+
 export const onSignIn = () => AsyncStorage.setItem(USER_KEY, "true");
 
 export const onSignOut = () => AsyncStorage.removeItem(USER_KEY);
--- app/screens/SignUp.js
+++ app/screens/SignUp.js
@@ -1,45 +1,23 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { Card, Button, FormLabel, FormInput } from "react-native-elements";
-import { onSignIn } from "../auth";
-
-// export default ({ navigation }) =>
-//   <View style={{ paddingVertical: 20 }}>
-//     <Card>
-//       <FormLabel>Email</FormLabel>
-//       <FormInput placeholder="Email address..." textInputRef="email" />
-//       <FormLabel>Password</FormLabel>
-//       <FormInput secureTextEntry placeholder="Password..." />
-//       <FormLabel>Confirm Password</FormLabel>
-//       <FormInput secureTextEntry placeholder="Confirm Password..." />
-//       {/*<UselessTextInputMultiline />*/}
-//       <Button
-//         buttonStyle={{ marginTop: 20 }}
-//         backgroundColor="#03A9F4"
-//         title="SIGN UP"
-//         onPress={() => {
-//           console.log(this.refs.forminput.refs.email);
-//           onSignIn().then(() => navigation.navigate("SignedIn"));
-//         }}
-//       />
-//       <Button
-//         buttonStyle={{ marginTop: 20 }}
-//         backgroundColor="transparent"
-//         textStyle={{ color: "#bcbec1" }}
-//         title="Sign In"
-//         onPress={() => navigation.navigate("SignIn")}
-//       />
-//     </Card>
-//   </View>;
+import { onSignIn, auth } from "../auth";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: "",
-      password: ""
+      email: "",
+      password: "",
+      cpassword: ""
     };
   }
+
+  login = () => {
+    console.log("this.state.email = ", this.state.email);
+    console.log("this.state.password = ", this.state.password);
+    console.log("this.state.cpassword = ", this.state.cpassword);
+  };
 
   render() {
     const navigation = this.props.navigation;
@@ -47,18 +25,34 @@ export default class App extends Component {
       <View style={{ paddingVertical: 20 }}>
         <Card>
           <FormLabel>Email</FormLabel>
-          <FormInput placeholder="Email address..." textInputRef="email" />
+          <FormInput
+            placeholder="Email address..."
+            onChangeText={email => this.setState({ email })}
+          />
           <FormLabel>Password</FormLabel>
-          <FormInput secureTextEntry placeholder="Password..." />
+          <FormInput
+            secureTextEntry
+            placeholder="Password..."
+            onChangeText={password => this.setState({ password })}
+          />
           <FormLabel>Confirm Password</FormLabel>
-          <FormInput secureTextEntry placeholder="Confirm Password..." />
+          <FormInput
+            secureTextEntry
+            placeholder="Confirm Password..."
+            onChangeText={cpassword => this.setState({ cpassword })}
+          />
           {/*<UselessTextInputMultiline />*/}
           <Button
             buttonStyle={{ marginTop: 20 }}
             backgroundColor="#03A9F4"
             title="SIGN UP"
             onPress={() => {
-              onSignIn().then(() => navigation.navigate("SignedIn"));
+              auth(this.state.email, this.state.password);
+              {
+                /*onSignIn().then(() =>
+                navigation.navigate("SignedIn")
+              );*/
+              }
             }}
           />
           <Button
@@ -73,36 +67,6 @@ export default class App extends Component {
     );
   }
 }
-
-// class UselessTextInputMultiline extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       text: "",
-//       password: ""
-//     };
-//   }
-
-// If you type something in the text box that is a color, the background will change to that
-// color.
-//   render(password) {
-//     return (
-//       <View
-//         style={{
-//           backgroundColor: "#ffffff",
-//           borderBottomColor: "#000000",
-//           borderBottomWidth: 1
-//         }}
-//       >
-//         <FormInput
-//           secureTextEntry
-//           placeholder="Confirm Password..."
-//           onChangeText={password => this.setState({ password })}
-//         />
-//       </View>
-//     );
-//   }
-// }
 
 // <View>
 //   <Tile
